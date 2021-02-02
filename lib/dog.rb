56e64c19e1311d16b4c274f5172d93cf7a148665
dapeshcode <ampeccia@gmail.com>
2021-02-01 21:08:34 -0600

    commit tree db3222ac3f8b0c5afd5aabf29778114c9d9c86b1

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog 
+    num_of_legs = 4
+    sound = "woof"
+    dog_type = "cartoon"
+end 
+
+fido = Dog.new 
+snoopy = Dog.new
+lassie = Dog.new  
+
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+    num_of_legs = 2
+    sound = "hello"
+    type = "alien"
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
